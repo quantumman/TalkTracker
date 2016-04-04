71ca6d533c1c4a6bc974bf44cf4dad08f5611abc
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-04 20:33:41 +0900

    "Initial Commit"

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('./style/index.scss');
+require('./index.html?output=index.html');
+/*===== yeoman style require hook =====*/
+function lazyLoad() {
+    var onload = window.onload || function () {
+    };
+    window.onload = function () {
+        var currentRoute = m.route();
+        Object.keys(routes).forEach(function (key) {
+            var route = routes[key];
+            if (key !== currentRoute)
+                route.controller();
+        });
+        onload();
+    };
+}
+var routes = {};
+m.route(document.body, '/', routes);
