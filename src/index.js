2e882da0836ee395c4f8fde0a23d78d632ca0919
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-10 22:16:49 +0900

    index.js: Do not load lazy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-require('./style/index.scss');
-require('./index.html?output=index.html');
-/*===== yeoman style require hook =====*/
-function lazyLoad() {
-    var onload = window.onload || function () {
-    };
-    window.onload = function () {
-        var currentRoute = m.route();
-        Object.keys(routes).forEach(function (key) {
-            var route = routes[key];
-            if (key !== currentRoute)
-                route.controller();
-        });
-        onload();
-    };
-}
-var routes = {};
+import './style/index.scss';
+import './index.html?output=index.html';
+
+const routes = {};
+
 m.route(document.body, '/', routes);
