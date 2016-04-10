8295193f5e5b5685b9188184a037a989ab93f551
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-10 23:05:43 +0900

    index.js: Import semantic ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style/index.scss';
 import './index.html?output=index.html';
+import '../semantic/dist/semantic.min.css';
+import '../semantic/dist/semantic.min.js';
 
 const routes = {};
 
