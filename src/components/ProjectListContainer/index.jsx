d84266ddc1e51fd2348b4749e8485854905658c8
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 00:13:10 +0900

    ProjectListContainer: Fetch projects

--- src/components/ProjectListContainer/index.jsx
+++ src/components/ProjectListContainer/index.jsx
@@ -1,11 +1,13 @@
 import ProjectList from '../ProjectList';
 
 import AuthenticatedUser from '../../models/AuthenticatedUser';
+import Project from '../../models/Project';
 
 export default {
   controller() {
     return {
       user: AuthenticatedUser.get(),
+      projects: Project.retrieve(),
     };
   },
 
