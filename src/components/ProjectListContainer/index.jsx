d1607f78f5d66af0ce06b270f9ac453dfcbc0343
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-11 23:22:52 +0900

    ProjectListContainer: Fetch authenticated user

--- src/components/ProjectListContainer/index.jsx
+++ src/components/ProjectListContainer/index.jsx
@@ -1,7 +1,12 @@
 import ProjectList from '../ProjectList';
 
+import AuthenticatedUser from '../../models/AuthenticatedUser';
+
 export default {
   controller() {
+    return {
+      user: AuthenticatedUser.get(),
+    };
   },
 
   view(ctrl) {
