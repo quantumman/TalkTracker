740cd212d344b5aeea245cca4e72593676981391
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 08:59:02 +0900

    IconMenuList: Show menu

--- src/components/ui/IconMenuList/index.jsx
+++ src/components/ui/IconMenuList/index.jsx
@@ -7,6 +7,24 @@ export default {
   view() {
     return (
       <div class="icon-menu-list">
+        <div class="ui vertical labeled icon menu">
+          <a class="item">
+            <i class="line chart icon"></i>
+            Activity
+          </a>
+          <a class="item">
+            <i class="comment icon"></i>
+            Messages
+          </a>
+          <a class="item">
+            <i class="tasks icon"></i>
+            Tasks
+          </a>
+          <a class="item">
+            <i class="setting icon"></i>
+            Setting
+          </a>
+        </div>
       </div>
     );
   },
