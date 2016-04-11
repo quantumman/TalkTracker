171c27c703a46ee80a7458eeeb900e03c11d7286
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-11 23:37:17 +0900

    Nav: Show search box

--- src/components/ui/Nav/index.jsx
+++ src/components/ui/Nav/index.jsx
@@ -12,6 +12,12 @@ export default {
         <div class="ui small menu">
           <div class="right menu">
             <div class="item">
+              <div class="ui icon input">
+                <input class="search" type="text" placeholder="Search..." />
+                <i class="search icon"></i>
+              </div>
+            </div>
+            <div class="item">
               <img class="ui avatar image" src={props.user().avatar} />
               <span>{props.user().name}</span>
             </div>
