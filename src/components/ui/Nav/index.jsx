97b31721647db3b021c03eff15344349889f7569
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-11 09:23:56 +0900

    Nav: Wrap content with container

--- src/components/ui/Nav/index.jsx
+++ src/components/ui/Nav/index.jsx
@@ -1,12 +1,17 @@
 import './style.scss';
 
+import Container from '../Container';
+
 export default {
   controller() {
   },
 
-  view() {
+  view(_ctrl, _props, children) {
     return (
       <div class="nav">
+        <Container>
+          {children}
+        </Container>
       </div>
     );
   },
