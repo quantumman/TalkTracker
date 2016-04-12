8a3806e73d09b6a0cc0aeb414807e52d59969575
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 09:22:49 +0900

    index.js: Define routing to messages

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import '../semantic/dist/semantic.min.js';
 
 import ProjectListContainer from './components/ProjectListContainer';
 import ActivityPageContainer from './components/ActivityPageContainer';
+import MessagesPageContainer from './components/ActivityPageContainer';
 
 const routes = {
   '/': ProjectListContainer,
   '/activity': ActivityPageContainer,
+  '/messages': MessagesPageContainer,
 };
 
 m.route(document.body, '/', routes);
