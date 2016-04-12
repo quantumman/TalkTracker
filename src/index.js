c4dc7c392b116cfb1552531f40f8b91fbffaa115
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 09:27:12 +0900

    index.js: Define routing to tasks

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import '../semantic/dist/semantic.min.js';
 import ProjectListContainer from './components/ProjectListContainer';
 import ActivityPageContainer from './components/ActivityPageContainer';
 import MessagesPageContainer from './components/ActivityPageContainer';
+import TasksPageContainer from './components/TasksPageContainer';
 
 const routes = {
   '/': ProjectListContainer,
   '/activity': ActivityPageContainer,
   '/messages': MessagesPageContainer,
+  '/tasks': TasksPageContainer,
 };
 
 m.route(document.body, '/', routes);
