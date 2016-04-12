be3f5694086897b3af54c34f48662b5bd2a15394
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 09:30:57 +0900

    index.js: Define routing to setting

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import ProjectListContainer from './components/ProjectListContainer';
 import ActivityPageContainer from './components/ActivityPageContainer';
 import MessagesPageContainer from './components/ActivityPageContainer';
 import TasksPageContainer from './components/TasksPageContainer';
+import SettingPageContainer from './components/SettingPageContainer';
 
 const routes = {
   '/': ProjectListContainer,
   '/activity': ActivityPageContainer,
   '/messages': MessagesPageContainer,
   '/tasks': TasksPageContainer,
+  '/setting': SettingPageContainer,
 };
 
 m.route(document.body, '/', routes);
