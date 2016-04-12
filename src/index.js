749d25de879fb9af74ea2faf19d2ad9e9aaa1bfd
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-12 09:05:34 +0900

    index.js: Define routing to activity

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import '../semantic/dist/semantic.min.css';
 import '../semantic/dist/semantic.min.js';
 
 import ProjectListContainer from './components/ProjectListContainer';
+import ActivityPageContainer from './components/ActivityPageContainer';
 
 const routes = {
   '/': ProjectListContainer,
+  '/activity': ActivityPageContainer,
 };
 
 m.route(document.body, '/', routes);
