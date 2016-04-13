9250412f1916f35f7d14c2cc4259f78aeda69d02
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-13 09:05:41 +0900

    BaseViewModel: Fetch projects

--- src/components/BaseViewModel/index.js
+++ src/components/BaseViewModel/index.js
@@ -1,8 +1,10 @@
 import AuthenticatedUser from '../../models/AuthenticatedUser';
+import Project from '../../models/Project';
 
 class BaseViewModel {
   constructor() {
     this.user = AuthenticatedUser.get();
+    this.projects = Project.retrieve();
   }
 }
 
