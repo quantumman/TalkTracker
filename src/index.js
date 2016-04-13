8d7c25328adc5c68fb83c254f8f317ce70b0630c
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-13 09:22:42 +0900

    ProjectsPage: Handle component state by view model

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './index.html?output=index.html';
 import '../semantic/dist/semantic.min.css';
 import '../semantic/dist/semantic.min.js';
 
-import ProjectsPageContainer from './components/ProjectsPageContainer';
+import ProjectsPage from './components/ProjectsPage';
 import ActivityPage from './components/ActivityPage';
 import MessagesPage from './components/ActivityPage';
 import TasksPage from './components/TasksPage';
 import SettingPage from './components/SettingPage';
 
 const routes = {
-  '/': ProjectsPageContainer,
+  '/': ProjectsPage,
   '/activity': ActivityPage,
   '/messages': MessagesPage,
   '/tasks': TasksPage,
