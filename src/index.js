ff88cdd91b885489bca1c1edfffda2c5e096581f
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-13 09:16:35 +0900

    index.js: Load TasksPage for /tasks

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import '../semantic/dist/semantic.min.js';
 import ProjectsPageContainer from './components/ProjectsPageContainer';
 import ActivityPage from './components/ActivityPage';
 import MessagesPage from './components/ActivityPage';
-import TasksPageContainer from './components/TasksPageContainer';
+import TasksPage from './components/TasksPage';
 import SettingPageContainer from './components/SettingPageContainer';
 
 const routes = {
   '/': ProjectsPageContainer,
   '/activity': ActivityPage,
   '/messages': MessagesPage,
-  '/tasks': TasksPageContainer,
+  '/tasks': TasksPage,
   '/setting': SettingPageContainer,
 };
 
