ad436822b2de7ce8e08e8d4688d6ab6f27498d11
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-13 09:04:16 +0900

    BaseViewModel: Fetch authenticated user

--- src/components/BaseViewModel/index.js
+++ src/components/BaseViewModel/index.js
@@ -1,4 +1,9 @@
+import AuthenticatedUser from '../../models/AuthenticatedUser';
+
 class BaseViewModel {
+  constructor() {
+    this.user = AuthenticatedUser.get();
+  }
 }
 
 export default new BaseViewModel();
