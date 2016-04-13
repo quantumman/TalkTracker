3da3c7577be075247ac9fbf813d4f54314c1d162
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-13 22:13:18 +0900

    Project: Fetch also events

--- src/models/Project.js
+++ src/models/Project.js
@@ -6,9 +6,21 @@ export default {
     return m.request({ method: 'POST', url: this.url, data: { name, description } });
   },
   retrieve() {
-    return m.request({ method: 'GET', url: this.url });
+    return m.request({ method: 'GET', url: this.url, initialValue: [] })
+            .then(ps => ps.map(p => {
+              this.events(p);
+              p.events = this.events(p); // eslint-disable-line no-param-reassign
+              return p;
+            }));
   },
   update({ id }) {
     return m.request({ method: 'PATCH', url: `api/v1/projects/${id}` });
+  },
+  events(project) {
+    return m.request({
+      method: 'GET',
+      url: project.events_url,
+      initialValue: [],
+    });
   },
 };
