2770bf8455e8702ffbd6cf0471470dd0fe5c7639
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-16 11:56:40 +0900

    Pagination: Render pager

--- src/components/ui/Pagination/index.jsx
+++ src/components/ui/Pagination/index.jsx
@@ -1,12 +1,87 @@
 import './style.scss';
 
+function range(start, last) {
+  const array = [];
+  for (let i = start; i < last; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+class ViewModel {
+  constructor({ page, link }) {
+    this.page = page();
+    this.pageNumbers = this.buildPageNumbers(this.page, parseInt(link.last.page, 10));
+  }
+
+  buildPageNumbers(page, lastpage) {
+    // [1]  2   3  ...  10
+    if (page === 1) {
+      return [1, 2, 3, '...', lastpage];
+    }
+
+    //  1  [2]  3  ...  10
+    //  1   2  [3]  4  ... 10
+    //  1   2   3  [4]  5  ... 10
+    if (page >= 2 && page <= 4) {
+      const pages = range(1, page + 2);
+      return pages.concat(['...', lastpage]);
+    }
+
+    //  1  ...  4  [5]  6  ... 10
+    //  1  ...  5  [6]  7  ... 10
+    if (page >= 5 && page <= lastpage - 4) {
+      return [
+        1,
+        '...',
+        page - 1,
+        page,
+        page + 1,
+        '...',
+        lastpage,
+      ];
+    }
+
+    //  1  ...  6  [7]  8  9   10
+    //  1  ...  7  [8]  9  10
+    //  1  ...  8  [9]  10
+    //  1  ...  8   9  [10]
+    if (page > lastpage - 4) {
+      const start = lastpage === page
+                  ? page - 2
+                  : page - 1;
+      const pages = range(start, lastpage + 1);
+      return ([1, '...']).concat(pages);
+    }
+
+    return undefined;
+  }
+}
+
 export default {
-  controller() {
+  controller(props) {
+    return new ViewModel(props);
   },
 
-  view() {
+  view(ctrl, props) {
     return (
-      <div class="pagination">
+      <div class="ui pagination menu">
+        {
+          ctrl.pageNumbers.map(n => {
+            const active = n === ctrl.page ? 'active' : '';
+
+            return n === '...'
+                 ? <div class="item disabled">{n}</div>
+                 : (
+                   <a class={`${active}  item`}
+                      href={`${props.href}?page=${n}`}
+                      config={m.route}
+                   >
+                     {n}
+                   </a>
+                 );
+          })
+        }
       </div>
     );
   },
