2c70c5f6fc4c994a520b247e363697af9e6e7c4d
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-18 08:24:16 +0900

    Project: Add messages

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,4 +1,15 @@
 import m from 'mithril';
+import parse from 'parse-link-header';
+
+const linkHeader = xhr => {
+  if (xhr.status === 200) {
+    const link = parse(xhr.getResponseHeader('Link'));
+    const response = JSON.parse(xhr.responseText);
+    return JSON.stringify({ data: response, link });
+  }
+
+  return xhr.responseText;
+};
 
 export default {
   url: 'api/v1/projects',
@@ -22,5 +33,8 @@ export default {
       url: project.events_url,
       initialValue: [],
     });
+  },
+  messages(project, args) {
+    return m.request({ method: 'GET', url: project.messages_url, data: args, extract: linkHeader });
   },
 };
