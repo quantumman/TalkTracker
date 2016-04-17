210474642bb290c9d335ded0dd386e45d1ccaa82
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-18 08:23:27 +0900

    MessagesPage: Show page menu

--- src/components/MessagesPage/index.jsx
+++ src/components/MessagesPage/index.jsx
@@ -1,21 +1,52 @@
 import './style.scss';
 
 import BasePage from '../BasePage';
+import Pagination from '../ui/Pagination';
+
+import Project from '../../models/Project';
 
 import BaseViewModel from '../BaseViewModel';
 
 class ViewModel extends BaseViewModel {
+  constructor() {
+    super();
+
+    this.pageNumber = m.prop(1);
+  }
+
+  init() {
+    this.pageNumber(parseInt(m.route.param('page') || '1', 10));
+    this.messages = this.current
+                        .then(p => Project.messages(p, this.pageNumber))
+                        .then(this.messages);
+  }
 }
+
+const vm = new ViewModel();
 
 export default {
   controller() {
-    return new ViewModel();
+    vm.init();
+    return vm;
   },
 
   view(ctrl) {
     return (
       <BasePage {...ctrl}>
         <div class="messages-page">
+          <div class="ui grid">
+            <div class="column">
+            </div>
+          </div>
+          <div class="ui two column centered grid">
+            <div class="column">
+              <Pagination page={ctrl.pageNumber}
+                          link={ctrl.messages().link}
+                          href={`/${ctrl.current().id}/messages`}
+              >
+              </Pagination>
+            </div>
+          </div>
         </div>
       </BasePage>
     );
