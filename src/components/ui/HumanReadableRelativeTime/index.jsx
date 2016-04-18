f930a1944dd90cfb4e1f5f3dd5573ff5d91984a7
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-19 08:17:19 +0900

    HumanReadableRelativeTime: Add fromNow

--- src/components/ui/HumanReadableRelativeTime/index.jsx
+++ src/components/ui/HumanReadableRelativeTime/index.jsx
@@ -1,5 +1,16 @@
+import moment from 'moment';
+
 export default {
   controller() {
+    return {
+      fromNow(time) {
+        const now = moment.utc();
+        const d = moment(time).utc();
+        return now.diff(d, 'weeks') === 0
+             ? moment(time).fromNow()
+             : moment(time).format('LL');
+      },
+    };
   },
 
   view() {
