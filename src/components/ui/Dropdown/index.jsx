84d57c4f5df268a96e7c93fd32ae1555dcd5893f
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-20 08:45:26 +0900

    Dropdown: Initialize dropdown

--- src/components/ui/Dropdown/index.jsx
+++ src/components/ui/Dropdown/index.jsx
@@ -2,12 +2,20 @@ export default {
   controller(props) {
     return {
       label: props.label,
+
+      config: (element, isInitialized) => {
+        if (isInitialized) {
+          $(element).dropdown('refresh');
+        } else {
+          $(element).dropdown();
+        }
+      },
     };
   },
 
   view(ctrl) {
     return (
-      <div class="ui floating pointing dropdown labeled icon button">
+      <div class="ui floating pointing dropdown labeled icon button" config={ctrl.config}>
         <i class="filter icon"></i>
         <span class="text">{ctrl.label}</span>
       </div>
