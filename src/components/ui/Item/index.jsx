03d6d68767ef0ba0f5899d84ad951e63a715a294
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-26 08:55:08 +0900

    Item: Render an item for dropdown menu

--- src/components/ui/Item/index.jsx
+++ src/components/ui/Item/index.jsx
@@ -1,10 +1,24 @@
 export default {
-  controller() {
+  controller(props) {
+    return {
+      value: props.value,
+      text: props.text,
+      color: props.color,
+    };
   },
 
-  view() {
+  view(ctrl) {
     return (
-      <div>
+      <div class="item"
+           data-value={ctrl.value}
+           data-text={ctrl.text}
+      >
+        {
+          ctrl.color
+          ? <div class={`ui ${ctrl.color} empty circular label`}></div>
+          : ''
+        }
+        {ctrl.text}
       </div>
     );
   },
