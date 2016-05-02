597e529c460449f9f2a28de4e5f8d7a944013927
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-02 22:01:37 +0900

    Dropdown: Search items optionally

--- src/components/ui/Dropdown/index.jsx
+++ src/components/ui/Dropdown/index.jsx
@@ -1,9 +1,16 @@
+import assign from 'object-assign';
+
 export default {
   controller(props) {
+    const defaultSeachProp = {
+      enabled: typeof(props.search) !== 'undefined',
+      placeholder: '',
+      icon: <i class="search icon"></i>,
+    };
     return {
       label: props.label,
-      placeholder: props.placeholder || '',
       name: props.name,
+      search: assign({}, defaultSeachProp, props.search),
 
       config: (element, isInitialized) => {
         if (isInitialized) {
@@ -22,10 +29,16 @@ export default {
         <i class="filter icon"></i>
         <span class="text">{ctrl.label}</span>
         <div class="menu">
-          <div class="ui icon search input">
-            <i class="search icon"></i>
-            <input type="text" placeholder={ctrl.placeholder} />
-          </div>
+          {
+            ctrl.search.enabled
+            ? (
+              <div class="ui icon search input">
+                {ctrl.search.icon}
+                <input type="text" placeholder={ctrl.search.placeholder} />
+              </div>
+            )
+            : ''
+          }
           <div class="scrolling menu">
             {children}
           </div>
