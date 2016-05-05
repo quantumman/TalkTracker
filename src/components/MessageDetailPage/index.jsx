9bd3068ee8aa35b2e87b11669a494c08a2538cec
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-05 19:24:48 +0900

    components: Add MessageDetailPage

---
+++ src/components/MessageDetailPage/index.jsx
@@ -0,0 +1,28 @@
+import './style.scss';
+
+import BasePage from '../BasePage';
+
+import BaseViewModel from '../BaseViewModel';
+
+class ViewModel extends BaseViewModel {
+  init() {
+  }
+}
+
+const vm = new ViewModel();
+
+export default {
+  controller() {
+    vm.init();
+    return vm;
+  },
+
+  view(ctrl) {
+    return (
+      <BasePage {...ctrl}>
+        <div class="message-detail-page">
+        </div>
+      </BasePage>
+    );
+  },
+};
