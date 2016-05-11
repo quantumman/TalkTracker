27cbfb7c4e5b3f2af99ae685d2fa0a319e8988a8
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-11 12:13:33 +0900

    Callout: Show callout

--- src/components/ui/Callout/index.jsx
+++ src/components/ui/Callout/index.jsx
@@ -4,9 +4,15 @@ export default {
   controller() {
   },
 
-  view() {
+  view(_ctrl, props, children) {
     return (
       <div class="callout">
+        <div class="ui segment">
+          <div class="ui top attached label callout--left">
+            {props.header}
+          </div>
+          <div>{children}</div>
+        </div>
       </div>
     );
   },
