39fe48c4c3fe1bb0484fe09b643b9a5f7e606333
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-13 10:29:02 +0900

    Message: Fix url of api

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,7 +2,7 @@ import m from 'mithril';
 
 export default {
   retrieve({ projectId, id }) {
-    return m.request({ method: 'GET', url: `api/v1/projects/${projectId}/${id}` })
+    return m.request({ method: 'GET', url: `api/v1/projects/${projectId}/messages/${id}` })
             .then(message => {
               message.comments = this.comments(message); // eslint-disable-line no-param-reassign
               return message;
