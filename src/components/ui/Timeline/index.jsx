7abe9a1d656b938a788ceaf4b9e58cf148218c63
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-13 10:29:56 +0900

    Timeline: Render commets on timeline

--- src/components/ui/Timeline/index.jsx
+++ src/components/ui/Timeline/index.jsx
@@ -1,12 +1,29 @@
-import 'style.scss';
+import './style.scss';
+
+import Callout from '../Callout';
 
 export default {
   controller() {
   },
 
-  view() {
+  view(_ctrl, props) {
+    const comments = props.comments || m.prop([]);
+
     return (
       <div class="timeline">
+        <div class="timeline-bar"></div>
+        {
+          comments().map(c =>
+            <div>
+              <img class="ui avatar image" src={c.creator.avatar} />
+              <div class="callout">
+                <Callout header={c.created_at}>
+                  {c.body}
+                </Callout>
+              </div>
+            </div>
+          )
+        }
       </div>
     );
   },
