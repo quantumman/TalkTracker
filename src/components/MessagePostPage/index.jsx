2f0b8dd3c0b801f5a3bdccbafc9c25480d3258f8
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-18 10:03:55 +0900

    MessagePostPage: Add editorMenu

--- src/components/MessagePostPage/index.jsx
+++ src/components/MessagePostPage/index.jsx
@@ -11,6 +11,51 @@ class ViewModel extends BaseViewModel {
 
 const vm = new ViewModel();
 
+const editorMenu = (
+  <div class="ui icon menu">
+    <a class="item">
+      <i class="bold icon"></i>
+    </a>
+    <a class="item">
+      <i class="italic icon"></i>
+    </a>
+    <a class="item">
+      <i class="strikethrough icon"></i>
+    </a>
+    <a class="item">
+      <i class="linkify icon"></i>
+    </a>
+
+    <div class="item">
+    </div>
+
+    <a class="item">
+      <i class="quote right icon"></i>
+    </a>
+    <a class="item">
+      <i class="code icon"></i>
+    </a>
+    <a class="item">
+      <i class="list icon"></i>
+    </a>
+    <a class="item">
+      <i class="ordered list icon"></i>
+    </a>
+    <a class="item">
+      <i class="outdent icon"></i>
+    </a>
+
+    <div class="right menu">
+      <a class="item">
+        <i class="arrow left icon"></i>
+      </a>
+      <a class="item">
+        <i class="arrow right icon"></i>
+      </a>
+    </div>
+  </div>
+);
+
 export default {
   controller() {
     vm.init();
