58171d5d5f147de7db5a6168408a932a5d035305
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-18 19:59:27 +0900

    Icon: Refactor - Improve coding style

--- src/components/ui/Icon/index.jsx
+++ src/components/ui/Icon/index.jsx
@@ -11,7 +11,11 @@ export default {
 
   view(ctrl, props) {
     return (
-      <a class="item" data-content={props.tooltip} data-variation="inverted" config={ctrl.config}>
+      <a class="item"
+         data-content={props.tooltip}
+         data-variation="inverted"
+         config={ctrl.config}
+      >
         <i class={`${props.icon} icon`}></i>
       </a>
     );
