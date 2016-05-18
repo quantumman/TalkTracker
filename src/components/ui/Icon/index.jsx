0e8a682c1db36cd9f937229d153656c8e3d71317
Shodai Yokoyama <quantumcars@gmail.com>
2016-05-18 19:58:36 +0900

    Icon: Invert tooltip color

--- src/components/ui/Icon/index.jsx
+++ src/components/ui/Icon/index.jsx
@@ -11,7 +11,7 @@ export default {
 
   view(ctrl, props) {
     return (
-      <a class="item" data-content={props.tooltip} config={ctrl.config}>
+      <a class="item" data-content={props.tooltip} data-variation="inverted" config={ctrl.config}>
         <i class={`${props.icon} icon`}></i>
       </a>
     );
